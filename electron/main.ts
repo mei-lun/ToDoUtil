7c761b5430ee698c68b6796a1f9afb90c53f3ed5
meilun <meilun0125@qq.com>
2026-06-23 11:40:05 +0800

    feat(electron): bootstrap main process with frameless window

---
+++ electron/main.ts
@@ -0,0 +1,39 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'node:path'
+
+const isDev = !app.isPackaged
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 280,
+    height: 600,
+    frame: false,
+    transparent: false,
+    resizable: true,
+    minWidth: 240,
+    minHeight: 360,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: false,
+    },
+  })
+
+  if (isDev && process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+    win.webContents.openDevTools({ mode: 'detach' })
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'))
+  }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
